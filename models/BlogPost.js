7c33034824e2969cbf0660e17db11605893ff581
remrem <vallejo.rw@gmail.com>
2023-10-06 16:05:52 +0800

    hello

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 // this script will create the schema of the database
 const BlogPostSchema = new Schema({
   title: String,
-  body: String
-})
+  body: String,
+});
 
-console.log('HIIII WALA KAY LAMI');
+console.log("HIIII WALA KAY LAMI");
 
-const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+console.log("hello han mas wala kay lamio");
+
+const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
 
 module.exports = BlogPost;
